28cc4d1ef2b9ca1564d9845dbd87c8482180f2ae
JULIAN TANAUSU VILLAR VAZQUEZ <cxb0204@hi>
2019-03-26 09:49:40 +0100

    initial commit

---
+++ app/src/main/java/com/example/castsound/CastOptionsProvider.java
@@ -0,0 +1,32 @@
+package com.example.castsound;
+
+import android.content.Context;
+
+import com.google.android.gms.cast.framework.CastOptions;
+import com.google.android.gms.cast.framework.OptionsProvider;
+import com.google.android.gms.cast.framework.SessionProvider;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CastOptionsProvider implements OptionsProvider {
+
+    //This is the custom namespace we will use to communicate with the cast receiver application
+    public static final String NAMESPACE = "urn:x-cast:mbanje.kurt.soundboard";
+
+    @Override
+    public CastOptions getCastOptions(Context context) {
+
+        List<String> supportedNamespaces = new ArrayList<>();
+        supportedNamespaces.add(NAMESPACE);
+        CastOptions castOptions = new CastOptions.Builder()
+                .setReceiverApplicationId(context.getString(R.string.app_id))
+//                .setSupportedNamespaces(supportedNamespaces)
+                .build();
+        return castOptions;
+    }
+    @Override
+    public List<SessionProvider> getAdditionalSessionProviders(Context context) {
+        return null;
+    }
+}
---
+++ app/src/main/java/com/example/castsound/CastSessionListener.java
@@ -0,0 +1,51 @@
+package com.example.castsound;
+
+import com.google.android.gms.cast.framework.Session;
+import com.google.android.gms.cast.framework.SessionManagerListener;
+
+public class CastSessionListener implements SessionManagerListener {
+    @Override
+    public void onSessionStarting(final Session session) {
+
+    }
+
+    @Override
+    public void onSessionStarted(final Session session, final String s) {
+
+    }
+
+    @Override
+    public void onSessionStartFailed(final Session session, final int i) {
+
+    }
+
+    @Override
+    public void onSessionEnding(final Session session) {
+
+    }
+
+    @Override
+    public void onSessionEnded(final Session session, final int i) {
+
+    }
+
+    @Override
+    public void onSessionResuming(final Session session, final String s) {
+
+    }
+
+    @Override
+    public void onSessionResumed(final Session session, final boolean b) {
+
+    }
+
+    @Override
+    public void onSessionResumeFailed(final Session session, final int i) {
+
+    }
+
+    @Override
+    public void onSessionSuspended(final Session session, final int i) {
+
+    }
+}
